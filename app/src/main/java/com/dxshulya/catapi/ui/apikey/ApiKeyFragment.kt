974ba2a1f111ea3a8bba56a7163fbd3021ea279d
dponomareva <daraonelove@yandex.ru>
2022-01-10 17:08:21 +0300

    add validator

--- app/src/main/java/com/dxshulya/catapi/ui/apikey/ApiKeyFragment.kt
+++ app/src/main/java/com/dxshulya/catapi/ui/apikey/ApiKeyFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.dxshulya.catapi.R
+import com.dxshulya.catapi.ui.login.LoginFragmentDirections
+import com.dxshulya.catapi.ui.main.CatFragmentDirections
 
 class ApiKeyFragment : Fragment() {
 
@@ -15,6 +18,9 @@ class ApiKeyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_api_key, container, false)
+
+
+
         return view
     }
 }
--- app/src/main/java/com/dxshulya/catapi/ui/login/LoginFragment.kt
+++ app/src/main/java/com/dxshulya/catapi/ui/login/LoginFragment.kt
@@ -6,23 +6,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import com.dxshulya.catapi.LoginValid
 import com.dxshulya.catapi.R
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
-import io.reactivex.rxjava3.core.BackpressureStrategy
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.schedulers.Schedulers
 
 class LoginFragment : Fragment() {
+
+    private lateinit var nextButton: Button
+    private lateinit var email: TextInputEditText
+    private lateinit var description: TextInputEditText
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        nextButton = view!!.findViewById(R.id.next_button_email)
+        nextButton.isEnabled = false
+        email = view.findViewById(R.id.email)
+        description = view.findViewById(R.id.description)
+        val edList = arrayOf(email, description)
+        val textWatcher = LoginValid(edList = edList, nextButton)
+        for (editText in edList) editText.addTextChangedListener(textWatcher)
+
+        nextButton.setOnClickListener{
+            val action = LoginFragmentDirections.actionLoginFragmentToApiKeyFragment()
+            Navigation.findNavController(view).navigate(action)
+        }
+
         return view
     }
 
